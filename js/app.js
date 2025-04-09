ab8f6d531d9217a1cbfa480ced8ab6e4efd50ce6
Cesar Flores <cesar.flores@mail.com>
2025-04-09 16:42:55 -0600

    Se agregaron id´s al index y se empezó a trabajar el archivo app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const ingreso = [
+  new Ingreso("Salario", 2100.0),
+  new Ingreso("Venta coche", 1500),
+];
+const egresos = [
+  new Egresos("Renta departamento", 900),
+  new Egresos("Ropa", 400),
+];
+
+let cargarApp = () => {
+  cargarCabecero();
+};
+
+let cargarCabecero = () => {};
